31aedacae59d7397973c8b0d0f5c2f718e8c35af
Jon <jonathon_rylatt@yahoo.com>
2023-12-06 21:57:52 +0800

    Added the favorite number
    My favorite number is 20

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            Console.WriteLine("Guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput < favNumber) 
+            {
+                Console.WriteLine("You guessed too low.");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("You guessed too high.");
+            }
+            else
+            {
+                Console.WriteLine("Ayyyy, you got it!");
+            }
+            
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+            //I'm not totally sure why, and google can't seem to be helping, but my command window auto closes in VS code so I have to add likes like the above two
         }
     }
 }
